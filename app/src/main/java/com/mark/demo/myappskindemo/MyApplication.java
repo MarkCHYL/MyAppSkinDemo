56482e189717331d51124ca38a4ad2e3f93b14a6
Mark <2285581945@qq.com>
2017-11-10 15:38:05 +0800

    Mark

---
+++ app/src/main/java/com/mark/demo/myappskindemo/MyApplication.java
@@ -0,0 +1,25 @@
+package com.mark.demo.myappskindemo;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
+
+/**
+ * Created by Mark on 2017/11/10.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initSkinView();
+    }
+
+    private void initSkinView() {
+
+    }
+
+    private void recreate() {
+
+    }
+}
